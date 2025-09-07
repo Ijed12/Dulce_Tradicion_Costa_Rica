69032905c1bd46acc2d84c1853814f38d63a0474
Ijed12 <156212331+Ijed12@users.noreply.github.com>
2025-09-06 22:33:28 -0600

    script.js

--- script.js
+++ script.js
@@ -14,32 +14,26 @@ const productData = {
     sponge: {
         title: 'Pan de banano',
         description: 'Un bizcocho artesanal preparado con bananos en su punto justo de madurez, integrados en una masa ligera y equilibrada. Horneado con calma, conserva una textura húmeda y una fragancia cálida que envuelve cada rebanada. Una preparación sencilla, pero con un carácter reconfortante y hogareño.',
-        image: 'assets2/66.PNG'
     },
     cheesecake: {
         title: 'Red Velvet',
         description: 'Suave, ligero y de un rojo intenso, el Red Velvet nace de la mezcla equilibrada de cacao, mantequilla y azúcar. Su horneado controlado garantiza una miga esponjosa, mientras que el glaseado de queso crema aporta frescura y contraste. Un pastel refinado, tan atractivo a la vista como al paladar.',
-        image: 'assets2/77.png'
-    },
+   },
     cupcake: {
         title: 'Cupcakes',
         description: 'Cada cupcake es una obra en miniatura: una masa suave horneada en moldes delicados, lista para recibir un toque final de crema o glaseado. Ya sea en versiones de banano, vainilla o chocolate, estos bocados combinan estética y sabor en perfecta armonía, pensados para acompañar tanto un momento casual como una ocasión especial.',
-        image: 'assets2/99.PNG'
     },
     macaron: {
         title: 'Croissant',
         description: 'Un clásico francés elaborado con masa hojaldrada trabajada a mano y mantequilla de calidad. Su proceso de laminado crea capas finas y aireadas, que tras el horneado ofrecen una corteza crujiente y un corazón tierno. Un pan que combina sencillez con sofisticación en cada bocado.',
-        image: 'assets2/15.PNG'
     },
     mousse: {
         title: 'Churros',
         description: 'Elaborados con una masa ligera, los churros se fríen en aceite caliente hasta alcanzar un dorado perfecto. Su interior es esponjoso y su superficie se realza con un delicado baño de azúcar y canela. Servidos recién hechos, evocan tradición y calidez en su forma más simple y delicios.',
-        image: 'assets2/16.PNG'
     },
     bento: {
         title: 'Donas',
         description: 'Preparadas a partir de una masa fermentada que reposa para ganar ligereza, las donas se fríen hasta lograr un acabado dorado y aireado. Posteriormente se bañan en glaseados finos de chocolate, vainilla o azúcar, ofreciendo un contraste perfecto entre suavidad y dulzor. Un clásico que nunca pierde su encanto.',
-        image: 'assets2/6.PNG'
     }
 };
 
@@ -78,6 +72,7 @@ window.addEventListener('click', (event) => {
     }
 
 });
+
 
 
 
